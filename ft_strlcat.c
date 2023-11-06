961b9b28f84d6520264f5ff768d02f9e96bb6c38
Gwendal - <gwendaaaaaaal@gmail.com>
2023-11-07 00:42:08 +0100

    Libc done

--- ft_strlcat.c
+++ ft_strlcat.c
@@ -6,20 +6,21 @@
 /*   By: marvin <marvin@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/07 04:39:08 by gholloco          #+#    #+#             */
-/*   Updated: 2023/10/25 12:01:00 by gholloco         ###   ########.fr       */
+/*   Updated: 2023/10/28 05:50:44 by gholloco         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-size_t ft_strlcat(char *dst, const char *src, size_t size)
+size_t	ft_strlcat(char *dst, const char *src, size_t size)
 {
-	int i, j;
+	int	i;
+	int	j;
 
 	i = 0;
 	j = 0;
 	if (size <= 0)
-		return (0); 
+		return (0);
 	while (dst[i])
 		i++;
 	while (src[j] && size > 0)
